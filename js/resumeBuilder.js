d1841d5a647ae4ad750405fefb33158f23e20fe5
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-05-29 13:41:44 -0500

    Adding formatted role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,5 +6,9 @@ console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(funThoughts);
 //$("#main").append(funThoughts);
-var formattedName = HTMLheaderName.replace("%data%", firstname);
+
+var formattedName = HTMLheaderName.replace("%data%", "Arturo Escobedo");
+var formattedRole = HTMLheaderRole.replace("%data%", "Informatics and Robotics Expert");
+
 $("#header").append(formattedName);
+$("#header").append(formattedRole);
