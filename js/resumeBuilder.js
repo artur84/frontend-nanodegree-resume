3e0c94d4a5b79352dd4149551e44a8965cc463f9
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-05-29 13:38:21 -0500

    Adding my name to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,10 @@
-$("#main").append("Arturo Escobedo");
+//$("#main").append("Arturo Escobedo");
 var firstname = "Pilinya";
 console.log(firstname);
 var awesomeThoughts = "I am Arturo and I am AWESOME!";
 console.log(awesomeThoughts);
 var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 console.log(funThoughts);
+//$("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", firstname);
+$("#header").append(formattedName);
