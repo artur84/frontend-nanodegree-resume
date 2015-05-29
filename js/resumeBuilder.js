80d7b9eea6e63a0ac2add5c035118e6c24075322
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2015-05-29 13:22:24 -0500

    funthoughts excersice completed

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,5 @@ var firstname = "Pilinya";
 console.log(firstname);
 var awesomeThoughts = "I am Arturo and I am AWESOME!";
 console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
